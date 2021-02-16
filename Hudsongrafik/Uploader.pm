f30e8d6eb84f6ec21f48b0bbaff90fe8a269c7ef
Amy Hicox <amy@hicox.com>
2021-02-15 19:58:02 -0600

    test2

--- Hudsongrafik/Uploader.pm
+++ Hudsongrafik/Uploader.pm
@@ -5,14 +5,27 @@ use warnings;
 
 use Apache2::RequestRec ();
 use Apache2::RequestIO ();
+use Apache2::Request;
 
 use Apache2::Const -compile => qw(OK);
 
 sub handler {
-    my $r = shift;
+    my $r = Apache2::Request->new(
+      shift,
+      POST_MAX => 10 * 1024 * 1024, # in bytes, so 10M
+      DISABLE_UPLOADS => 0
+    );
 
     $r->content_type('text/plain');
     print "mod_perl 2.0 rocks!\n";
+
+    foreach my $upload ($r->upload) {
+      my $filename  = $upload->filename;
+      my $filehandle = $upload->fh;
+      my $size    = $upload->size;
+
+      $r->print("You sent me a file named $filename, $size bytes<br>");
+    }
 
     return Apache2::Const::OK;
 }
